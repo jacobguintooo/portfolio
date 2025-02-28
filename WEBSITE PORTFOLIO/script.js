9e2341fa868345d56d9389c9b6ef018ce18b9501
jacobguintooo <jacobguintoxd@gmail.com>
2025-02-28 09:35:29 +0800

    Delete WEBSITE PORTFOLIO directory

--- WEBSITE PORTFOLIO/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        alert('Message sent!');
-        form.reset();
-    });
-});
